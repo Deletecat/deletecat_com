c992b7bd5804617d6b47fe9c92cdf2119082d835
Deletecat <55948500+Deletecat@users.noreply.github.com>
2023-09-12 19:51:49 +0100

    Final edit, hopefully D:

--- firefish/rss.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-FILE=updated
-if [ -f "$FILE" ]; then
-    git config --local user.email "41898282+github-actions[bot]@users.noreply.github.com"
-    git config --local user.name "github-actions[bot]"
-    git add -A
-    git commit -m "Add changes"
-    git push
-fi
